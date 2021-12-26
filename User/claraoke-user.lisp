6dd1309676e8b4eaf8f6a6fba725df8cdded02e5
Panji Kusuma <epanji@gmail.com>
2021-12-26 09:01:32 +0700

    Make color parameter more flexible

--- User/claraoke-user.lisp
+++ User/claraoke-user.lisp
@@ -10,38 +10,41 @@
                                    :if-does-not-exist if-does-not-exist)
     (print-script object stream)))
 
+(defun html-color-code (input)
+  (let ((c (color input)))
+    (format nil "#~2,'0X~2,'0X~2,'0X" (red c) (green c) (blue c))))
+
 (defun claraoke-subtitle-dummy-video (object filespec &optional (color "#008000"))
   (check-type filespec pathname)
   (if (probe-file filespec)
-      (format t "File ~A already exists." (namestring filespec))
+      (format t "~&File ~A already exists.~%" (namestring filespec))
       (let* ((max (apply 'max (loop for event in (lines (events object))
                                     collect (durationinteger (end event)))))
-             (width (value (find-info object "PlayResX")))
-             (height (value (find-info object "PlayResY")))
              (ffmpeg (format nil "ffmpeg -lavfi color=c=~A:s=~Dx~D -t ~D ~S"
-                             color
-                             width
-                             height
+                             (html-color-code color)
+                             (value (find-info object "PlayResX"))
+                             (value (find-info object "PlayResY"))
                              (ceiling max 100)
                              (namestring filespec))))
         (uiop:run-program ffmpeg :force-shell t)))
   filespec)
 
-(defun claraoke-subtitle-hardsub-video (subtitle &optional video-or-color)
+(defun claraoke-subtitle-hardsub-video (subtitle &optional (video-or-color "#008000"))
   (check-type subtitle pathname)
-  (check-type video-or-color (or null pathname string))
   (let* ((object (parse-script subtitle))
          (max (apply 'max (loop for event in (lines (events object))
                                 collect (durationinteger (end event)))))
-         (width (value (find-info object "PlayResX")))
-         (height (value (find-info object "PlayResY")))
          (input (if (pathnamep video-or-color)
                     (format nil "-i ~S -lavfi ass=~S" (namestring video-or-color) (namestring subtitle))
                     (format nil "-lavfi color=c=~A:s=~Dx~D,ass=~S -t ~D"
-                            (or video-or-color "#008000")
-                            width height (namestring subtitle) (ceiling max 100))))
-         (output (translate-pathname subtitle "*.*" "hardsub.mp4")))
+                            (html-color-code video-or-color)
+                            (value (find-info object "PlayResX"))
+                            (value (find-info object "PlayResY"))
+                            (namestring subtitle)
+                            (ceiling max 100))))
+         (output (translate-pathname subtitle "*.*" "*-hardsub.mp4")))
     (if (probe-file output)
-        (format t "File ~A already exists." (namestring output))
-        (uiop:run-program (format nil "ffmpeg ~A ~S" input (namestring output)) :force-shell t))))
+        (format t "~&File ~A already exists.~%" (namestring output))
+        (uiop:run-program (format nil "ffmpeg ~A ~S" input (namestring output)) :force-shell t))
+    output))
 
