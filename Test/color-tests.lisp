f1dfd7e37e7d85a6e0e89811748b36dc164a4061
Panji Kusuma <epanji@gmail.com>
2025-10-29 18:08:35 +0000

    Color integer tests

--- Test/color-tests.lisp
+++ Test/color-tests.lisp
@@ -4,7 +4,10 @@
 
 (test color-from-integer
   (is (colorp (rgb 12 34 56)))
-  (is (colorp (rgb 12 34 56 77))))
+  (is (colorp (rgb 12 34 56 77)))
+  (is (colorp (color 305419895)))
+  (is (colorp (color -305419895)))
+  (is (colorp (color #x4D38220C))))
 
 (test color-from-string-name
   (is (colorp (color "Wrong name lead to zero color")))
@@ -67,7 +70,47 @@
   (is (string= (alphastring "&HA&") (alphastring "&HAA&")))
   (is (string= (alphastring "&HA") (alphastring "&HAA"))))
 
+(test increase-decrease-color
+  (let ((mutable1 (color "green"))
+        (mutable2 (color "yellow")))
+    (increase-color mutable1 "red")
+    (is (= (colorinteger mutable1) (colorinteger mutable2))))
+  (let ((mutable1 (color "green"))
+        (mutable2 (color "yellow")))
+    (decrease-color mutable2 "red")
+    (is (= (colorinteger mutable1) (colorinteger mutable2))))
+  (is (= (colorinteger (increase-color "green" "red"))
+         (colorinteger "yellow")))
+  (is (= (colorinteger (decrease-color "yellow" "red"))
+         (colorinteger "green"))))
+
+(test bitwise-color
+  (is (= (colorinteger "&H1100BBAA")
+         (colorinteger (bitwise-color :and "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "&H22220000")
+         (colorinteger (bitwise-color :andc1 "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "&HCCCC0000")
+         (colorinteger (bitwise-color :andc2 "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "&HEEEE0001")
+         (colorinteger (bitwise-color :eqv "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "&HFFEEBBAA")
+         (colorinteger (bitwise-color :ior "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "&H1100BBAB")
+         (colorinteger (bitwise-color :nand "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "&HFFEEBBAB")
+         (colorinteger (bitwise-color :nor "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "&HCCCC0001")
+         (colorinteger (bitwise-color :orc1 "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "&H22220001")
+         (colorinteger (bitwise-color :orc2 "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "&HEEEE0000")
+         (colorinteger (bitwise-color :xor "&HDDCCBBAA" "&H3322BBAA"))))
+  (is (= (colorinteger "yellow")
+         (colorinteger (bitwise-color #'+ "green" "red"))))
+  (is (= (colorinteger "green")
+         (colorinteger (bitwise-color #'- "yellow" "red")))))
+
 (test expecting-color-error
-  (signals error (colorstring 1234567))
-  (signals error (colorstring #x8A2BE2)))
+  (signals error (colorstring 1234567.89))
+  (signals error (colorstring #c(123 456))))
 
